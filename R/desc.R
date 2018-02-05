ac608d4a5d75a5fed8ffa947238112a637fbb84d
Peter M C Harrison <pmc.harrison@gmail.com>
2018-02-05 14:33:14 +0000

    Rename

--- R/desc.R
+++ R/desc.R
@@ -1,6 +1,6 @@
 #' @export
-setGeneric("get_chord_freqs", function(x) standardGeneric("get_chord_freqs"))
-setMethod("get_chord_freqs", signature(x = "Corpus"),
+setGeneric("get_chord_counts", function(x) standardGeneric("get_chord_counts"))
+setMethod("get_chord_counts", signature(x = "Corpus"),
           function(x) {
             x2 <- declass(x)
             events <- do.call(c, x2)
--- tests/testthat/test-get_chord_freqs.R
+++ tests/testthat/test-get_chord_freqs.R
@@ -1,11 +1,11 @@
-context("get_chord_freqs")
+context("get_chord_counts")
 
 test_that("toy example", {
   d <- as.Corpus(list(
     a = c(1, 1, 2, 3, 1),
     b = c(3, 1, 4, 5)
   ))
-  res <- get_chord_freqs(d)
+  res <- get_chord_counts(d)
   expect_equal(names(res), as.character(1:5))
   expect_equal(as.numeric(res),
                c(4, 1, 2, 1, 1))
