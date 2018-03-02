2d0fdd5bf9ce3a84fe95533435ab6e19db7fe461
Peter M C Harrison <pmc.harrison@gmail.com>
2018-03-02 19:06:56 +0000

    Updating datasets to new specification

--- data-raw/_import-corpora.R
+++ data-raw/_import-corpora.R
@@ -1,5 +1,4 @@
 library(HarmonyUtils)
-library(hash)
 library(magrittr)
 library(jsonlite)
 library(plyr)
@@ -11,9 +10,21 @@ collapse_chords <- function(x) {
   })
 }
 
+as_chords <- function(composition_list) {
+  lapply(composition_list,
+         function(composition) {
+           lapply(composition,
+                  function(chord) {
+                    x <- chord %% 12L
+                    new_chord(bass_pc = x[1], non_bass_pc_set = x[- 1])
+                  })
+         })
+}
+
 message("Importing classical dataset")
 classical <- read_json("data-raw/classical.json") %>%
   collapse_chords %>%
+  as_chords %>%
   llply(., encode_chords, .progress = "text") %>%
   as.harmony_corpus
 use_data(classical, overwrite = TRUE)
@@ -21,6 +32,7 @@ use_data(classical, overwrite = TRUE)
 message("Importing popular dataset")
 popular <- read_json("data-raw/popular.json") %>%
   collapse_chords %>%
+  as_chords %>%
   llply(., encode_chords, .progress = "text") %>%
   as.harmony_corpus
 use_data(popular, overwrite = TRUE)
@@ -28,6 +40,7 @@ use_data(popular, overwrite = TRUE)
 message("Importing jazz dataset")
 jazz <- read_json("data-raw/jazz.json") %>%
   collapse_chords %>%
+  as_chords %>%
   llply(., encode_chords, .progress = "text")  %>%
   as.harmony_corpus
 use_data(jazz, overwrite = TRUE)
---
+++ tests/testthat/test-legacy_consistency.R
@@ -0,0 +1,8 @@
+context("legacy_consistency")
+
+test_that("examples", {
+  # expect_equal(
+  #   HarmonyCorpora::classical
+  #   c(14375, 8298, 12325, 14375, 4317)
+  # )
+})
